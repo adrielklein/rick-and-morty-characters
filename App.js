131ceba4aa5332f64ff552fe481016d758c9dcee
Adriel Klein <adrielmklein@gmail.com>
2023-01-30 12:05:15 -0600

    add image

--- App.js
+++ App.js
@@ -1,36 +1,53 @@
 import { StatusBar } from "expo-status-bar";
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
-  Button,
   TouchableOpacity,
   FlatList,
   RefreshControl,
+  ActivityIndicator,
+  Image,
 } from "react-native";
 
 export default function App() {
   const [characters, setCharacters] = useState([]);
-  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [nextPageUrl, setNextPageUrl] = useState();
   const [refreshing, setRefreshing] = useState(false);
   const loadData = useCallback(() => {
-    fetch("https://rickandmortyapi.com/api/character")
+    fetch(`https://rickandmortyapi.com/api/character`)
       .then((response) => response.json())
       .then((json) => {
-        console.log("lala", json.results);
         setCharacters(json.results);
+        setNextPageUrl(json.info.next);
         setRefreshing(false);
       })
       .catch((error) => console.error(error));
-  }, [setCharacters, setRefreshing]);
+  }, [setCharacters, setRefreshing, setNextPageUrl]);
+
+
+  const handleLoadMore = useCallback(() => {
+    if (!nextPageUrl) {
+      return;
+    }
+    setLoadingMore(true);
+    fetch(nextPageUrl)
+      .then((response) => response.json())
+      .then((json) => {
+        setNextPageUrl(json.info.next);
+        setCharacters([...characters, ...json.results]);
+      })
+      .catch((error) => console.error(error));
+    setLoadingMore(false);
+  }, [setCharacters, setNextPageUrl, setLoadingMore, nextPageUrl, characters]);
   const onResetTap = useCallback(() => {
     loadData();
   }, [loadData]);
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
-    // Perform data load here
     loadData();
     setRefreshing(false);
   }, [loadData, setRefreshing]);
@@ -38,29 +55,35 @@ export default function App() {
   const renderItem = ({ item }) => {
     return (
       <View style={styles.rowContainer}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: item.image,
+          }}
+        />
         <Text style={styles.rowText}>{item.name}</Text>
         <Text style={styles.rowText}>{item.status}</Text>
-        <Text style={styles.rowText}>{item.name}</Text>
       </View>
     );
   };
+
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.titleText}>Rick and Morty Characters</Text> */}
+      <Text style={styles.titleText}>Rick and Morty Characters</Text>
       <TouchableOpacity style={styles.resetButton} onPress={onResetTap}>
         <Text style={styles.resetButtonText}>Reset</Text>
       </TouchableOpacity>
       <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Image</Text>
         <Text style={styles.headerText}>Name</Text>
         <Text style={styles.headerText}>Status</Text>
-        <Text style={styles.headerText}>Height</Text>
       </View>
       <FlatList
         data={characters}
         renderItem={renderItem}
         keyExtractor={(character) => character.id}
-        // onEndReached={handleLoadMore}
-        // onEndReachedThreshold={0.5}
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={1}
         ListEmptyComponent={
           <View style={{ alignItems: "center", padding: 20 }}>
             <Text>Pull to load data</Text>
@@ -68,6 +91,13 @@ export default function App() {
         }
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        ListFooterComponent={
+          loadingMore && (
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+          )
         }
       />
 
@@ -86,6 +116,7 @@ const styles = StyleSheet.create({
   titleText: {
     fontSize: 20,
     fontWeight: "bold",
+    marginBottom: 10,
   },
   headerContainer: {
     backgroundColor: "#f2f2f2",
@@ -120,5 +151,9 @@ const styles = StyleSheet.create({
   resetButtonText: {
     color: "white",
     fontWeight: "bold",
+  },
+  image: {
+    height: 50,
+    width: 50,
   },
 });
